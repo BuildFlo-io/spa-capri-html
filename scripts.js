b80cc8e8436e063f959b6b42c2985e9f98b72d62
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-03-17 07:54:24 -0700

    adjust classes added or removed on scroll

--- scripts.js
+++ scripts.js
@@ -15,10 +15,10 @@ document.addEventListener('DOMContentLoaded', function () {
 window.addEventListener('scroll', function () {
   const nav = document.querySelector('nav');
   if (window.scrollY > 0) {
-    nav.classList.add('bg-white', 'shadow-sm');
+    nav.classList.add('bg-white', 'shadow-lg');
     nav.classList.remove('bg-transparent');
   } else {
-    nav.classList.remove('bg-white', 'shadow-sm');
+    nav.classList.remove('bg-white', 'shadow-lg');
     nav.classList.add('bg-transparent');
   }
 });
