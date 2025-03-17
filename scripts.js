0324d785a341b9383756049ea094be4aaf7a5695
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-03-17 08:33:29 -0700

    update scripts

--- scripts.js
+++ scripts.js
@@ -11,14 +11,22 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-
 window.addEventListener('scroll', function () {
   const nav = document.querySelector('nav');
+  const isHomePage = document.body.classList.contains('home') ||
+    window.location.pathname === '/' ||
+    window.location.pathname === '/index.html' ||
+    window.location.pathname === '/index.php';
+
   if (window.scrollY > 0) {
     nav.classList.add('bg-white', 'shadow-lg');
     nav.classList.remove('bg-transparent');
   } else {
-    nav.classList.remove('bg-white', 'shadow-lg');
-    nav.classList.add('bg-transparent');
+    if (!isHomePage) {
+      nav.classList.add('shadow-lg');
+    } else {
+      nav.classList.remove('bg-white', 'shadow-lg');
+      nav.classList.add('bg-transparent');
+    }
   }
 });
