d3dada66115cc38fecfe8a212048fc34b2867f3e
Charles <15911636+charlesGraham@users.noreply.github.com>
2025-03-17 07:43:50 -0700

    add script from WP instance

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const nav = document.querySelector('nav');
+
+  const isHomePage = document.body.classList.contains('home') ||
+    window.location.pathname === '/' ||
+    window.location.pathname === '/index.html' ||
+    window.location.pathname === '/index.php';
+
+  if (!isHomePage) {
+    nav.classList.add('shadow-lg');
+  }
+});
+
+
 window.addEventListener('scroll', function () {
   const nav = document.querySelector('nav');
   if (window.scrollY > 0) {
